3ed5934c780f39451186596e7991319667bee0a7
aleksiakolo <aleksiakolo@gmail.com>
2025-12-20 00:08:58 -0500

    factoring script added

---
+++ R/apply_factors.R
@@ -0,0 +1,42 @@
+apply_factors <- function(tables) {
+
+    market_levels <- sort(unique(tables$Orders$market))
+    region_levels <- sort(unique(tables$Orders$region))
+
+    tables$Orders <- tables$Orders %>%
+        mutate(
+            ship_mode      = factor(ship_mode, levels = c("First Class", "Standard Class", "Second Class", "Same Day")),
+            segment        = factor(segment),
+            order_priority = factor(order_priority,levels = c("Low", "Medium", "High", "Critical"), mordered = TRUE),
+            category       = factor(category),
+            sub_category   = factor(sub_category),
+            market         = factor(market, levels = market_levels),
+            region         = factor(region, levels = region_levels),
+            country        = factor(country),
+            state          = factor(state)
+        )
+
+    tables$Returns <- tables$Returns %>%
+        mutate(
+            returned = as_integer(returned == "TRUE"),
+            market   = factor(market, levels = market_levels)
+        )
+
+    tables$People <- tables$People %>%
+        mutate(
+            region = factor(region, levels = region_levels)
+        )
+
+    tables
+}
+
+
+# source("R/apply_factors.R")
+
+# tables <- list(
+#   Orders   = readr::read_csv("data/raw_cleaned/Orders.csv"),
+#   Returns  = readr::read_csv("data/raw_cleaned/Returns.csv"),
+#   People   = readr::read_csv("data/raw_cleaned/People.csv")
+# )
+
+# tables <- apply_factors(tables)
